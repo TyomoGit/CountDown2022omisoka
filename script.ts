d86ee89ea777a61aafb3c79b27660d5b4938d089
TyomoGit <riku3dno.1@gmail.com>
2022-12-31 22:20:15 +0900

    あけおめメッセージを津追加

--- script.ts
+++ script.ts
@@ -2,13 +2,18 @@ const clock = document.getElementById("clock")!;
 const countDown = document.getElementById("countDown")!;
 
 function loop(): void {
-    const now = new Date();
+    // const now = new Date();
+    const now = new Date(2023,1-1,1);
     const happyNewYear = new Date(2023,1-1,1,0,0,0,0);
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const seconds = now.getSeconds().toString().padStart(2, "0");
     clock.innerHTML = `⏰${hours}:${minutes}:${seconds}`;
-    countDown.innerHTML = `<span style="font-size: 2rem;">新年まであと</span><br><span style="font-weight: 900;">${Math.trunc(happyNewYear.getTime()/1000 - now.getTime()/1000)}</span>秒`;
+    if(now.getTime() >= happyNewYear.getTime()){
+        countDown.innerHTML = "🎆🎍🎌<br>あけましておめでとう"
+    }else{
+        countDown.innerHTML = `<span style="font-size: 2rem;">新年まであと</span><br><span style="font-weight: 900;">${Math.trunc(happyNewYear.getTime()/1000 - now.getTime()/1000)}</span>秒`;
+    }
     window.requestAnimationFrame(loop);
 }
 
