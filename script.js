cb21fb141bdeba9a587ce859076381da8f91f55e
TyomoGit <riku3dno.1@gmail.com>
2022-12-31 21:24:28 +0900

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+const clock = document.getElementById("clock");
+const countDown = document.getElementById("countDown");
+function loop() {
+    const now = new Date();
+    const happyNewYear = new Date(2023, 1 - 1, 1, 0, 0, 0, 0);
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    clock.innerHTML = `⏰${hours}:${minutes}:${seconds}`;
+    countDown.innerHTML = `新年まであと<span style="font-weight: 900;">${Math.trunc(happyNewYear.getTime() / 1000 - now.getTime() / 1000)}</span>秒`;
+    window.requestAnimationFrame(loop);
+}
+loop();
